d3099b2405a89576e64cd029c5bf087ea9e60b16
JJ-223 <151877132+JJ-223@users.noreply.github.com>
2025-01-30 15:39:17 -0800

    Practicing Github functions with simple program

---
+++ Lab1/Hello_1.cpp/Hello_1.cpp/Hello_1.cpp.cpp
@@ -0,0 +1,14 @@
+// Hello_1.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{   
+    cout << "Hello World!\n";
+    printf("Hello World again!\n");
+    return 0;
+}
+
