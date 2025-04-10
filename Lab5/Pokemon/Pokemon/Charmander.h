bc97c8f3d164a3f8664f8a3a3c1fc205a61ec95a
JJ-223 <151877132+JJ-223@users.noreply.github.com>
2025-04-09 20:08:39 -0700

    Starting Lab5

---
+++ Lab5/Pokemon/Pokemon/Charmander.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef CHARMANDER_H
+#define CHARMANDER_H
+
+#include <string>
+#include <vector>
+#include "Pokemon.h"
+using namespace std;
+
+class Charmander
+{
+public:
+	Charmander();
+	Charmander(string name, int hp, int att, int def, vector<string> t, vector <string> s);
+
+	void speak();
+	void printStats();
+
+private:
+	vector<string> skills;
+};
+#endif
---
+++ Lab5/Pokemon/Pokemon/Pokemon.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Pokemon
+{
+public:
+	Pokemon();
+	Pokemon(string name, int hp, int att, int def, vector<string> type);
+
+	virtual void speak();
+	virtual void printState();
+
+protected:
+	string name;
+	int hp;
+	int attack;
+	int defense;
+	vector <string> type;
+
+};
+#endif
